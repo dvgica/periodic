383d34cdcf0fcef90ea1e3d1ed0ad255c813a39e
David van Geest <david.vangeest@gmail.com>
2023-10-15 22:53:31 -0400

    Naming.

--- periodic-api/src/main/scala/ca/dvgi/periodic/config.scala
+++ periodic-api/src/main/scala/ca/dvgi/periodic/config.scala
@@ -49,7 +49,7 @@ sealed trait UpdateInterval[-T] {
   def duration(t: T): FiniteDuration
 }
 object UpdateInterval {
-  case class Fixed(interval: FiniteDuration) extends UpdateInterval[Any] {
+  case class Static(interval: FiniteDuration) extends UpdateInterval[Any] {
     override def duration(t: Any): FiniteDuration = interval
   }
   case class Dynamic[T](calculateUpdateInterval: T => FiniteDuration) extends UpdateInterval[T] {
